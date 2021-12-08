19d18fa25daf6749239f6fcea105dd85f115f8c1
ajbose02 <ajbose02@gmail.com>
2021-12-08 14:08:59 -0500

    initial commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4gob0ecyq8sczqi3dx&*hu7d1vwif=1ttunmrdi^u#xy_3!2^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
